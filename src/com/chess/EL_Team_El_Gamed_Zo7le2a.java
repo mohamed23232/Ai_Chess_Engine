b3fd58ed4d987c7597d49cf53ea1c6fc5150e10b
Mohamed Rabea <mohamed23232>
2024-09-05 02:25:07 +0300

    first commit

---
+++ src/com/chess/EL_Team_El_Gamed_Zo7le2a.java
@@ -0,0 +1,10 @@
+package com.chess;
+
+import com.chess.gui.Table;
+
+public class EL_Team_El_Gamed_Zo7le2a {
+
+    public static void main(final String[] args) throws Exception {
+        Table.get().show();
+    }
+}
